5e748d17830998c3008c70136362fcd6bbcc77e3
Antoine Hoarau <hoarau.robotics@gmail.com>
2016-02-29 23:22:19 +0100

    new tests on width

--- js/grayscale.js
+++ js/grayscale.js
@@ -8,10 +8,8 @@
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $(".light").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        $(".light").removeClass("top-nav-collapse");
     }
 });
 
